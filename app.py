90e8f1da07780c3d597ffe37337fdab93e64597f
xinjie-long <xinjie.long.2023@scis.smu.edu.sg>
2025-03-12 21:24:11 +0800

    created html pages

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
+@app.route('/')
+def home():
+    return render_template('index.html')

176cc4bf9f0b470811a33619a38543e23e495d84
xinjie-long <xinjie.long.2023@scis.smu.edu.sg>
2025-03-12 21:33:08 +0800

    edited login page

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    return render_template('login.html')
